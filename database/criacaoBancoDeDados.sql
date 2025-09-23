169ea67a297873d2e291d01f49216cfdb2d38d3d
caio <caio.cav.ara@gmail.com>
2025-09-22 22:01:57 -0300

    Adição do script para criação do banco de dados 'banco_talentos'

---
+++ database/criacaoBancoDeDados.sql
@@ -0,0 +1,14 @@
+-- Database: banco_talentos
+
+-- DROP DATABASE IF EXISTS banco_talentos;
+
+CREATE DATABASE banco_talentos
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
